1fab51658b817399ef60913d6d8e81ca3508e126
Paul Mitchum <paul@mile23.com>
2012-12-13 19:57:52 -0800

    update to drupal 7.17

--- drupal/sites/all/modules/checklistapi/checklistapi.js
+++ drupal/sites/all/modules/checklistapi/checklistapi.js
@@ -1,31 +1,37 @@
 (function ($) {
-
   "use strict";
+
+  /**
+   * Provides the summary information for the block settings vertical tabs.
+   */
   Drupal.behaviors.checklistapiFieldsetSummaries = {
     attach: function (context) {
-
-      // Vertical tabs summaries.
       $('#checklistapi-checklist-form .vertical-tabs-panes > fieldset', context).drupalSetSummary(function (context) {
         var total = $(':checkbox.checklistapi-item', context).size(), args = {};
         if (total) {
           args['@complete'] = $(':checkbox.checklistapi-item:checked', context).size();
           args['@total'] = total;
           args['@percent'] = Math.round(args['@complete'] / args['@total'] * 100);
-          return Drupal.t('@complete of @total (@percent%) complete', args);
+          return Drupal.t('@complete of @total (@percent%)', args);
         }
       });
+    }
+  };
 
-      // Compact mode link.
+  /**
+   * Adds dynamic item descriptions toggling.
+   */
+  Drupal.behaviors.checklistapiCompactModeLink = {
+    attach: function (context) {
       $('#checklistapi-checklist-form .compact-link a', context).click(function () {
+        $(this).closest('#checklistapi-checklist-form').toggleClass('compact-mode');
         var is_compact_mode = $(this).closest('#checklistapi-checklist-form').hasClass('compact-mode');
         $(this)
-          .text((is_compact_mode) ? Drupal.t('Hide descriptions') : Drupal.t('Show descriptions'))
-          .attr('title', (is_compact_mode) ? Drupal.t('Compress layout by hiding descriptions.') : Drupal.t('Expand layout to include descriptions.'))
-          .closest('#checklistapi-checklist-form').toggleClass('compact-mode');
-        document.cookie = 'Drupal.visitor.checklistapi_compact_mode=' + ((is_compact_mode) ? 0 : 1);
+          .text((is_compact_mode) ? Drupal.t('Show item descriptions') : Drupal.t('Hide item descriptions'))
+          .attr('title', (is_compact_mode) ? Drupal.t('Expand layout to include item descriptions.') : Drupal.t('Compress layout by hiding item descriptions.'))
+        document.cookie = 'Drupal.visitor.checklistapi_compact_mode=' + ((is_compact_mode) ? 1 : 0);
         return false;
       });
-
     }
   };
 
